76cf823b8868b112162262f5b9bd865fa0407d0a
czh <1632365610@qq.com>
2020-04-23 18:52:29 +0800

    2.0版本UI  搜索 页面详情（话题详情，内容详情，视频详情 视频竖版横板，举报，删除 消息界面）侧滑删除，角标数字 1.0版本设置多点问题，选择无流量上传多次显示问题解决 视频编辑降速时间过大问题解决

--- app/src/main/java/com/kingyon/elevator/uis/actiivty2/HomeActivity.java
+++ app/src/main/java/com/kingyon/elevator/uis/actiivty2/HomeActivity.java
@@ -57,7 +57,7 @@ import cn.jpush.android.api.JPushInterface;
  * Created By Admin  on 2020/3/30
  * Email : 163235610@qq.com
  * Author:Myczh
- * Instructions:首页入口
+ * Instructions:首页入口宣传宣传宣传下下策
  */
 public class HomeActivity extends BaseActivity implements AMapLocationListener {
     @BindView(R.id.frame_content)
